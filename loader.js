a47a54dc73ea5a4bb76a85f46cb0209da16fe2e7
apocas <petermdias@gmail.com>
2013-12-30 21:39:34 +0000

    test profile

--- loader.js
+++ loader.js
@@ -32,19 +32,23 @@ Loader.prototype.run = function(command, args, options, plugin) {
   });
 
   d.run(function() {
-    //TODO: find test type
-    self.nonInteractive(command, args, options);
+    var guess = self.profileTest(env);
+    if(guess.interactive === true) {
+      self.interactive(command, args, options, guess.image);
+    } else {
+      self.nonInteractive(command, args, options, guess.image);
+    }
   });
 };
 
 
-Loader.prototype.nonInteractive = function(command, args, options) {
+Loader.prototype.nonInteractive = function(command, args, options, img) {
   var self = this;
   var cid = undefined;
   if(self.container) {
     cid = self.container.container.id;
   } else {
-    cid = 'browserswarm/nodejs';
+    cid = img;
   }
 
   var container = new Container(self.docker, self.server, command, args, options, cid);
@@ -67,12 +71,12 @@ Loader.prototype.nonInteractive = function(command, args, options) {
 };
 
 
-Loader.prototype.interactive = function(command, args, options) {
+Loader.prototype.interactive = function(command, args, options, img) {
   //TODO: refactoring needed...
   var self = this;
 
   if(!this.container) {
-    this.container = new Container(self.docker, self.server, 'TODO: CMD BOOT SHIM', options, 'browserswarm/sauce');
+    this.container = new Container(self.docker, self.server, 'TODO: CMD BOOT SHIM', options, img);
     container.create(function(err, dcontainer) {
       if (err) {
         console.log(err);
@@ -86,30 +90,49 @@ Loader.prototype.interactive = function(command, args, options) {
 
     container.on('started', function() {
       //TODO: inspect created container to find private IP
-      var socket = net.connect(3333);
-      var remote = DuplexEmitter(socket);
-      self.remote = remote;
+      var ip = "";
+      self.connectShim(ip);
 
-      remote.on('stderr', function(data) {
-        self.server.emit('stderr', data);
-      });
-
-      remote.on('stdout', function(data) {
-        self.server.emit('stdout', data);
-      });
-
-      remote.on('close', function(code) {
-        self.server.emit('close', code);
-        //TODO: prob disconnect client
-      });
+      self.remote.emit('spawn', command, args, options);
     });
 
     container.on('done', function(code) {
       self.images.push(this.container.id);
     });
+  } else {
+    self.remote.emit('spawn', command, args, options);
   }
+  
+};
 
-  self.remote.emit('spawn', command, args, options);
+
+Loader.prototype.profileTest = function(env) {
+  //TODO: strider's 'plugin' property changes during a test (plugin:git, plugin:sauce, ...), fix & refactor this later.
+  if(env.SAUCE_USERNAME) {
+    return {'interactive': true, 'image': 'browserswarm/sauce'};
+  } else {
+    return {'interactive': false, 'image': 'browserswarm/nodejs'};
+  }
+};
+
+
+Loader.prototype.connectShim = function(ip) {
+  var socket = net.connect({'host': ip, 'port': 3333});
+  var remote = DuplexEmitter(socket);
+  self.remote = remote;
+
+  remote.on('stderr', function(data) {
+    self.server.emit('stderr', data);
+  });
+
+  remote.on('stdout', function(data) {
+    self.server.emit('stdout', data);
+  });
+
+  remote.on('close', function(code) {
+    self.server.emit('close', code);
+    //TODO: prob disconnect client
+  });
 };
 
 
